213a14f9371a625f1648b222612557bfa3dc48d4
razztafari <rasti93@gmail.com>
2018-02-17 18:14:20 +0100

    Structured the code so that I could remove as many global variables as possible.

--- TrainingApp/app/src/main/java/kandidat/trainingapp/MainActivity.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/MainActivity.java
@@ -8,39 +8,32 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-
 //testing git again
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
     private final String TAG = "FB_SIGNIN";
     EditText editEmail, editPassword;
-    private FirebaseAuth mAuth;
+
     private FirebaseAuth.AuthStateListener mAuthListener;
-    FirebaseUser user;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.create_button).setOnClickListener(this);
-        findViewById(R.id.sign_button).setOnClickListener(this);
 
-        editEmail = findViewById(R.id.editEmail);
-        editPassword = findViewById(R.id.editPassword);
         mAuth = FirebaseAuth.getInstance();
 
-
-        //Firebase authListener used to check if user is or isn't signed in.
+        // Keeps track of if the user was signed in or not when the application was closed
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                user = firebaseAuth.getCurrentUser();
+                FirebaseUser user = firebaseAuth.getCurrentUser();
                 //If the user state is signed in show profile page.
                 if(user != null){
                     Log.d(TAG, "User signed in " + user.getUid());
@@ -52,6 +45,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         };
+
+        findViewById(R.id.create_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CreateActivity.class ));
+            }
+        });
+        findViewById(R.id.sign_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        editEmail = findViewById(R.id.editEmail);
+        editPassword = findViewById(R.id.editPassword);
     }
 
     @Override
@@ -88,11 +97,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-            if (password.isEmpty()) {
-                editPassword.setError("Please fill in the Password-field");
-                editPassword.requestFocus();
-                return;
-            }
+        if (password.isEmpty()) {
+            editPassword.setError("Please fill in the Password-field");
+            editPassword.requestFocus();
+            return;
+        }
 
             signInToAccount(email,password);
 
@@ -110,21 +119,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-
-            //When Sign In button is pressed
-            case R.id.sign_button:
-                signIn();
-                break;
-
-            // When Create account button is pressed
-            case R.id.create_button:
-                startActivity(new Intent(this,CreateActivity.class ));
-                break;
-        }
     }
 }
--- TrainingApp/app/src/main/java/kandidat/trainingapp/ProfileActivity.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/ProfileActivity.java
@@ -1,6 +1,7 @@
 package kandidat.trainingapp;
 
 import android.content.Intent;
+import android.icu.util.TimeZone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +15,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+public class ProfileActivity extends AppCompatActivity{
     private final String TAG = "FB_PROFILE";
-    private FirebaseAuth mAuth;
-    private DatabaseReference mrefUser;
-    private DatabaseReference mrefUsername;
-    private DatabaseReference mrefPoints;
+
     private String usernameCap;
     private TextView welcomeText,pointsText;
 
@@ -28,25 +26,38 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        findViewById(R.id.logOut_btn).setOnClickListener(this);
+
+        //A reference to Authentication in Firebase
+        final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        createWelcome( FirebaseDatabase.getInstance().getReference("users").child(mAuth.getUid()));
+
+        findViewById(R.id.logOut_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent loginIntent = new Intent(ProfileActivity.this, MainActivity.class);
+                loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(loginIntent);
+            }
+        });
+
         welcomeText = (TextView) findViewById(R.id.profile_text);
         pointsText = (TextView) findViewById(R.id.profile_points);
-        mAuth = FirebaseAuth.getInstance();
-        createWelcome( FirebaseDatabase.getInstance().getReference("users").child(mAuth.getUid()));
+
     }
 
     // Create customised welcomemessage with username
-    public void createWelcome(DatabaseReference userNameRef){
-        mrefUsername = userNameRef.child("username");
-        mrefPoints = userNameRef.child("points");
+    public void createWelcome(DatabaseReference userRef){
 
-        mrefUsername.addValueEventListener(new ValueEventListener() {
+       userRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String username = dataSnapshot.getValue(String.class);
+                UserInformation user = dataSnapshot.getValue(UserInformation.class);
+
                 //Capitalize username
-                usernameCap = username.substring(0,1).toUpperCase() + username.substring(1);
+                usernameCap = user.getUsername().substring(0,1).toUpperCase() + user.getUsername().substring(1);
                 welcomeText.setText("Welcome " +  usernameCap + "!");
+                pointsText.setText("You have collected " + user.getPoints() + " points." );
             }
 
             @Override
@@ -54,31 +65,5 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
             }
         });
-
-        mrefPoints.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Integer points = dataSnapshot.getValue(Integer.class);
-                pointsText.setText("You have collected " + points + " points." );
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.logOut_btn:
-                mAuth.signOut();
-                Intent loginIntent = new Intent(this, MainActivity.class);
-                loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(loginIntent);
-                break;
-        }
     }
 }
