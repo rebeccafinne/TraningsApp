48ba6e238fd9c6be59822b021d10cf675c905808
rebeccafinne <rebecca@finne.se>
2018-02-22 13:04:01 +0100

    Fixed name of class

--- TrainingApp/app/src/main/java/kandidat/trainingapp/trainingFragment.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/trainingFragment.java
@@ -19,21 +19,21 @@ import android.widget.TableRow;
  * create an instance of this fragment.
  */
 public class TrainingFragment extends Fragment {
- /*   // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    /*   // TODO: Rename parameter arguments, choose names that match
+       // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+       private static final String ARG_PARAM1 = "param1";
+       private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+       // TODO: Rename and change types of parameters
+       private String mParam1;
+       private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+       private OnFragmentInteractionListener mListener;
 
-    public TrainingFragment() {
-        // Required empty public constructor
-    }
-*/
+       public TrainingFragment() {
+           // Required empty public constructor
+       }
+   */
     public static TrainingFragment newInstance() {
         TrainingFragment fragment = new TrainingFragment();
         return fragment;
