feead5ad65b8cad90840addc59a5813189f383da
rebeccafinne <rebecca@finne.se>
2018-03-02 10:19:13 +0100

    Added two new toolbars depending on where in the app it is

--- TrainingApp/app/src/main/java/kandidat/trainingapp/AppMainActivity.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/AppMainActivity.java
@@ -1,5 +1,7 @@
 package kandidat.trainingapp;
 
+import android.app.IntentService;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,16 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.app.TabActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by rebeccafinne on 2018-02-22.
  */
 
-public class AppMainActivity extends AppCompatActivity {
+public class AppMainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private BottomNavigationView mBottomNav;
 
     private Toolbar toolbar;
+    private TextView toolText;
+    private ImageButton settingsButton;
     ActionBar actionBar;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +34,19 @@ public class AppMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_app_running);
         mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_fragment);
+        toolText = (TextView) toolbar.findViewById(R.id.toolbar_text);
+        settingsButton = (ImageButton) toolbar.findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(this);
+
+
+        toolText.setText("Workout");
+        settingsButton.setImageResource(R.drawable.settings_small);
+
+        // setSupportActionBar(toolbar);
         // toolbar = getSupportActionBar(); // or getActionBar();
-        getSupportActionBar().setTitle("BetterTogether");
-        //getSupportActionBar().setLogo(R.drawable.settingsbuttonwhite);
+        //getSupportActionBar().setTitle("BetterTogether");
+        //getSupportActionBar().setLogo(R.drawable.settings);
         
         mBottomNav.setSelectedItemId(R.id.action_workout);
 
@@ -42,21 +57,31 @@ public class AppMainActivity extends AppCompatActivity {
 
                 switch (item.getItemId()){
                     case R.id.action_me:
+                        toolText.setText("Me");
+
                         selectedFragment = ProfileFragment.newInstance();
+
                         break;
                     case R.id.action_workout:
+                        toolText.setText("Workout");
+
                         selectedFragment = TrainingFragment.newInstance();
                         break;
                     case R.id.action_favorites:
+
+                        toolText.setText("Favorites");
 
                         selectedFragment = FavoritesFragment.newInstance();
 
                         break;
                     case R.id.action_leaderboard:
+                        toolText.setText("Friends");
+
                         selectedFragment = LeaderboardFragment.newInstance();
 
                         break;
                 }
+
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.frame_layout, selectedFragment);
                 transaction.commit();
@@ -73,9 +98,18 @@ public class AppMainActivity extends AppCompatActivity {
 
 
 
+
+
         //Used to select an item programmatically
         //bottomNavigationView.getMenu().getItem(2).setChecked(true);
 
     }
 
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+
+    }
 }
