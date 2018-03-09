7978249b5fbe29e2a9d44ba795cbe83482be5fa6
rebeccafinne <rebecca@finne.se>
2018-03-09 16:09:29 +0100

    Added activities for adding favorites also started with adding stairs

--- TrainingApp/app/src/main/java/kandidat/trainingapp/AddFavoritesActivity.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/AddFavoritesActivity.java
@@ -1,5 +1,6 @@
 package kandidat.trainingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -21,28 +22,35 @@ public class AddFavoritesActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar_activity);
         toolText = (TextView) toolbar.findViewById(R.id.activity_text);
-        toolText.setText("Add Favorite Workout");
+        toolText.setText("Favorite Workouts");
         gymButton = (Button) findViewById(R.id.add_gym);
         gymButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                gymClicked(view);
 
             }
         });
-        cardioButton = (Button) findViewById(R.id.add_gym);
-        cardioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
-        dailyButton = (Button) findViewById(R.id.add_gym);
+        dailyButton = (Button) findViewById(R.id.add_daily);
         dailyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                dailyClicked(view);
             }
         });
 
     }
+
+    private void gymClicked(View view){
+        Intent intent = new Intent(this, AddNewGymActiviy.class);
+        startActivity(intent);
+    }
+
+    private void dailyClicked(View view){
+        Intent intent = new Intent(this, AddNewDailyActivity.class);
+        startActivity(intent);
+
+    }
+
 }
--- TrainingApp/app/src/main/java/kandidat/trainingapp/AddNewDailyActivity.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/AddNewDailyActivity.java
@@ -1,13 +1,37 @@
 package kandidat.trainingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AddNewDailyActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TextView toolText;
+    Button addStairs, addBus, addStand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_daily);
+        toolbar = (Toolbar) findViewById(R.id.toolbar_activity);
+        toolText = (TextView) toolbar.findViewById(R.id.activity_text);
+        toolText.setText("Favorite daily exercise");
+        addStairs = (Button) findViewById(R.id.add_stairs);
+        addStairs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addStairsClicked(view);
+            }
+        });
+    }
+
+    private void addStairsClicked(View view){
+        Intent intent = new Intent(this, FavoriteStairsActivity.class);
+        startActivity(intent);
     }
 }
