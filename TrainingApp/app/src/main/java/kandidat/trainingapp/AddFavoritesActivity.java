d46bb7d024fcea9a44f96bd79500b1b5f159f757
rebeccafinne <rebecca@finne.se>
2018-03-09 14:21:31 +0100

    small fixes

--- TrainingApp/app/src/main/java/kandidat/trainingapp/AddFavoritesActivity.java
+++ TrainingApp/app/src/main/java/kandidat/trainingapp/AddFavoritesActivity.java
@@ -4,13 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class AddFavoritesActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     TextView toolText;
-
+    Button gymButton, cardioButton, dailyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,27 @@ public class AddFavoritesActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar_activity);
         toolText = (TextView) toolbar.findViewById(R.id.activity_text);
         toolText.setText("Add Favorite Workout");
+        gymButton = (Button) findViewById(R.id.add_gym);
+        gymButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        cardioButton = (Button) findViewById(R.id.add_gym);
+        cardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        dailyButton = (Button) findViewById(R.id.add_gym);
+        dailyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
